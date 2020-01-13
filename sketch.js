11bed94183de0565bcc02e84a3065a921204f056
Fleursikkink <55102193+Fleursikkink@users.noreply.github.com>
2020-01-13 15:13:46 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+function setup() {
+  // put setup code here
+  createCanvas (400,400);
+background(240); //grijs
+
+
+
+}
+
+function draw() {
+
+  // put drawing code here
+  
+  background (0);
+ 
+
+  noStroke();
+  fill (0,255,0);
+  rect (0,0,400-mouseX, 400 );
+
+
+ 
+
+}
